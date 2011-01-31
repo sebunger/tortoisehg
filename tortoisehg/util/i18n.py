1435bc5859c709bd61ef7c835f7f0bb28d6a055a
Ludovico Cavedon <cavedon@debian.org>
2011-01-30 18:40:53 -0800

    Imported Upstream version 1.1.8

--- tortoisehg/util/i18n.py
+++ tortoisehg/util/i18n.py
@@ -5,12 +5,52 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2, incorporated herein by reference.
 
-import gettext, sys
-from gettext import gettext as _
+import gettext, os, locale
+from mercurial import util
 from tortoisehg.util import paths
 
-gettext.bindtextdomain("tortoisehg", paths.get_locale_path())
-gettext.textdomain("tortoisehg")
+_localeenvs = ('LANGUAGE', 'LC_ALL', 'LC_MESSAGES', 'LANG')
+def _defaultlanguage():
+    if os.name != 'nt' or util.any(e in os.environ for e in _localeenvs):
+        return  # honor posix-style env var
+
+    # On Windows, UI language can be determined by GetUserDefaultUILanguage(),
+    # but gettext doesn't take it into account.
+    # Note that locale.getdefaultlocale() uses GetLocaleInfo(), which may be
+    # different from UI language.
+    #
+    # For details, please read "User Interface Language Management":
+    # http://msdn.microsoft.com/en-us/library/dd374098(v=VS.85).aspx
+    try:
+        from ctypes import windll  # requires Python>=2.5
+        langid = windll.kernel32.GetUserDefaultUILanguage()
+        return locale.windows_locale[langid]
+    except (ImportError, AttributeError, KeyError):
+        pass
+
+def setlanguage(lang=None):
+    """Change translation catalog to the specified language"""
+    global t
+    if not lang:
+        lang = _defaultlanguage()
+    opts = {}
+    if lang:
+        opts['languages'] = (lang,)
+    t = gettext.translation('tortoisehg', paths.get_locale_path(),
+                            fallback=True, **opts)
+setlanguage()
+
+def availablelanguages():
+    """List up language code of which message catalog is available"""
+    basedir = paths.get_locale_path()
+    def mopath(lang):
+        return os.path.join(basedir, lang, 'LC_MESSAGES', 'tortoisehg.mo')
+    langs = [e for e in os.listdir(basedir) if os.path.exists(mopath(e))]
+    langs.append('en')  # means null translation
+    return sorted(langs)
+
+def _(message):
+    return t.gettext(message)
 
 def agettext(message):
     """Translate message and convert to local encoding
